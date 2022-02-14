853eddf27605c63d8ff76e0a1f7267f9435d6726
shammi77 <nusratshammi.vutu@gmail.com>
2022-02-14 23:08:42 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    // string bananor jonno
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calc = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calc.value = '';
+        }
+
+    }
+    else {
+        const previousCalc = calc.value;
+        const newCalc = previousCalc + number;
+
+        calc.value = newCalc;
+    }
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const pass = document.getElementById('notify-pass');
+
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        pass.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+        pass.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
